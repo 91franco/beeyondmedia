231cad4b3a125a4ad5620ff5da1b863166637055
91franco <franco.guerrenio@gmail.com>
2023-03-02 23:59:01 -0300

    Validacion usuario

--- cypress/e2e/formSignIn.screen.js
+++ cypress/e2e/formSignIn.screen.js
@@ -1,4 +1,4 @@
-class FormContactUsScreen {
+class FormSignInScreen {
  
   get inputEmail(){
     return ('input[name="user"]');//'[class="kt-login__signin"]>[class="kt-form"]>[class="input-group"]>[placeholder="Email"]');
@@ -12,45 +12,6 @@ class FormContactUsScreen {
     return('#kt_login_signin_submit')
   }
 
-   get fieldMandatoryEmailMessageText() {
-    return (
-      '.grid > :nth-child(1) > p' 
-    );
-  }
-
-
-  get fieldMandatoryNameMessageText() {
-    return (
-      '.grid > :nth-child(1) > p'
-    );
-  }
-  get fieldMandatoryNameMessageTextcu() {
-    return (
-      'p[class="mt-0.5 right-0 text-xs text-firehydrant-50"]'
-    );
-  }
-
-  get fieldMandatoryCompanyMessageText() {
-    return (
-      '.grid > :nth-child(2) > p'
-    );
-  }
-  get fieldMandatoryCompanyMessageTextcu() {
-    return (
-      'p[class="mt-0.5 right-0 text-xs text-firehydrant-50"]'
-    );
-  }
-
-  async goToContactUs() {
-    /*cy.visit('contact')
-      /*'button[class="relative h-12 leading-none uppercase rounded-[4rem] font-medium flex items-center justify-center w-full bg-[length:200%_100%] overflow-hidden whitespace-nowrap bg-gradient-btn-white hover:text-white text-carnation tracking-[0.1875rem] bg-[right_bottom] transition-all duration-500 ease-in-out hover:bg-left-bottom max-w-[10.375rem] md:max-w-[12.25rem]"]').click();*/
-  }
-
-  async sendMail() {
-    cy.get(this.btnSend).click();
-  }
- 
-
 }
 
-module.exports = new FormContactUsScreen();
+module.exports = new FormSignInScreen();
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,8 +2,6 @@
 
 const FormContactUsScreen = require("../e2e/formSignIn.screen")
 
-const sendMail = true
-
 before(function () {
     //load the values from the file dataForm.json into the dataForm object
     cy.fixture('dataForm').then(function (dataForm) {
@@ -33,12 +31,12 @@ after(() => {
 })
 
 // Suite test cases
-describe('Test Cases Login', function () {
+describe('Test Cases SignIn', function () {
   
     //testcase1
-    it.only('Login - Submit (correct information)"', function () {
+    it.only('SignIn - Submit (incorrect information)"', function () {
         cy.allure().severity('critical');
-        cy.allure().tag('Login');
+        cy.allure().tag('SignIn');
        
         cy.allure()
         .startStep('Complete Email')
@@ -49,7 +47,7 @@ describe('Test Cases Login', function () {
         .endStep(cy.get(FormContactUsScreen.inputPassword).type(this.dataForm.password));
   
         cy.allure()
-        .startStep('Click Login')
+        .startStep('Click SignIn')
         .endStep(cy.get(FormContactUsScreen.loginBtn).click());
         
         cy.wait(4000)
@@ -59,196 +57,7 @@ describe('Test Cases Login', function () {
         .endStep(cy.contains(this.dataForm.messageIncorrectUser)       
            .should('contain.text', this.dataForm.messageIncorrectUser)
             .should('be.visible'));
-      
-        /*
-        cy.allure()
-        .startStep('Complete Company Name')
-        .endStep(cy.get(FormContactUsScreen.inputCompany).type(this.dataForm.company));
-        
-        cy.allure()
-        .startStep('Complete Phone Number')
-        .endStep(cy.get(FormContactUsScreen.inputPhone).type(this.dataForm.phone));
        
-        
-        cy.allure()
-        .startStep('Complete What service do you need?')
-        .endStep(cy.get(FormContactUsScreen.inputServicedev).select(this.dataForm.services.dev));
-           
-        cy.allure()
-        .startStep('Complete Note')
-        .endStep(cy.get(FormContactUsScreen.inputNotedev).type('Test form Contact us - Submit (correct information) - Service "' + this.dataForm.services.dev + '"'));
-       
-        if (sendMail === true) {
-            cy.allure()
-            .step('Validate Send')
-            .endStep(FormContactUsScreen.sendMail());
-            cy.wait(1000) //Ver como eliminar este paso
-            cy.allure()
-            .startStep('Validate Url Thanks')
-            .endStep(cy.url().should('be.equal', this.dataForm.urlthanks_dev));
-        }; */
     })
    
-    //testcase2
-    it('Contact us - Submit (Email address not completed)', function () {
-
-        cy.allure().severity('normal'); //'blocker' | 'critical' | 'normal' | 'minor' | 'trivial';
-        cy.allure().tag('ContactUs');
-        cy.allure()
-        .startStep('Contact-us')
-        .endStep(FormContactUsScreen.goToContactUs());
-
-        cy.allure()
-        .startStep('Complete Full Name')
-        .endStep(cy.get(FormContactUsScreen.inputFullName).type(this.dataForm.full_name_dev));
-
-        cy.allure()
-        .startStep('Complete Company Name')
-        .endStep(cy.get(FormContactUsScreen.inputCompany).type(this.dataForm.company));
-
-        cy.allure()
-        .startStep('Complete Phone Number')
-        .endStep(cy.get(FormContactUsScreen.inputPhone).type(this.dataForm.phone));
-
-        cy.allure()
-        .startStep('Complete What service do you need?')
-        .endStep(cy.get(FormContactUsScreen.inputServicedev).select(this.dataForm.services.dev));
-
-        cy.allure()
-        .startStep('Complete Note')
-        .endStep(cy.get(FormContactUsScreen.inputNotedev).type('Test form Contact us - Submit (correct information) - Service "' + this.dataForm.services.dev + '"'));
-        
-        cy.allure()
-        .step('Validate Send')
-        .endStep(FormContactUsScreen.sendMail());
-
-        cy.allure()
-        .step('Validate mandatory field Email')
-        .endStep(cy.get(FormContactUsScreen.fieldMandatoryEmailMessageTextcu)       
-           .should('contain.text', this.dataForm.messageFieldMandatory)
-            .should('be.visible'));
-
-    })
-    
-    //testcase3
-    it('Contact us - Submit (Full name not completed)', function () {
-
-        cy.allure().severity('normal'); //'blocker' | 'critical' | 'normal' | 'minor' | 'trivial';
-        cy.allure().tag('ContactUs');
-        cy.allure()
-        .startStep('Contact-us')
-        .endStep(FormContactUsScreen.goToContactUs());
-       
-        cy.allure()
-        .startStep('Complete Email Address')
-        .endStep(cy.get(FormContactUsScreen.inputEmail).type(this.dataForm.email_address));
-        
-        cy.allure()
-        .startStep('Complete Company Name')
-        .endStep(cy.get(FormContactUsScreen.inputCompany).type(this.dataForm.company));
-
-        cy.allure()
-        .startStep('Complete Phone Number')
-        .endStep(cy.get(FormContactUsScreen.inputPhone).type(this.dataForm.phone));
-
-        cy.allure()
-        .startStep('Complete What service do you need?')
-        .endStep(cy.get(FormContactUsScreen.inputServicedev).select(this.dataForm.services.dev));
-
-        cy.allure()
-        .startStep('Complete Note')
-        .endStep(cy.get(FormContactUsScreen.inputNotedev).type('Test form Contact us - Submit (correct information) - Service "' + this.dataForm.services.dev + '"'));
- 
-        cy.allure()
-        .step('Validate Send')
-        .endStep(FormContactUsScreen.sendMail());
-
-        cy.allure()
-        .step('Validate mandatory field Full name')
-        .endStep(cy.get(FormContactUsScreen.fieldMandatoryNameMessageTextcu)
-            .should('contain.text', this.dataForm.messageFieldMandatory)
-            .should('be.visible'));
-
-    })
-
-    //testcase4
-    it('Contact us - Submit (Company not completed)', function () {
-
-        cy.allure().severity('normal'); //'blocker' | 'critical' | 'normal' | 'minor' | 'trivial';
-        cy.allure().tag('ContactUs');
-        cy.allure()
-        .startStep('Contact-us')
-        .endStep(FormContactUsScreen.goToContactUs());
-
-        cy.allure()
-        .startStep('Complete Full Name')
-        .endStep(cy.get(FormContactUsScreen.inputFullName).type(this.dataForm.full_name_dev));
-
-        cy.allure()
-        .startStep('Complete Email Address')
-        .endStep(cy.get(FormContactUsScreen.inputEmail).type(this.dataForm.email_address));
-
-        cy.allure()
-        .startStep('Complete Phone Number')
-        .endStep(cy.get(FormContactUsScreen.inputPhone).type(this.dataForm.phone));
-
-        cy.allure()
-        .startStep('Complete What service do you need?')
-        .endStep(cy.get(FormContactUsScreen.inputServicedev).select(this.dataForm.services.dev));
-
-        cy.allure()
-        .startStep('Complete Note')
-        .endStep(cy.get(FormContactUsScreen.inputNotedev).type('Test form Contact us - Submit (correct information) - Service "' + this.dataForm.services.dev + '"'));
-   
-        cy.allure()
-        .step('Validate Send')
-        .endStep(FormContactUsScreen.sendMail());
-
-        cy.allure()
-        .step('Validate mandatory field Company')
-        .endStep(cy.get(FormContactUsScreen.fieldMandatoryCompanyMessageTextcu)
-            .should('contain.text', this.dataForm.messageFieldMandatory)
-            .should('be.visible'));
-    })
-
-    //testcase5
-    it('Contact us - Submit (correct information) - Complete only fields mandatory', function () {
-
-        cy.allure().severity('normal'); //'blocker' | 'critical' | 'normal' | 'minor' | 'trivial';
-        cy.allure().tag('ContactUs');
-        cy.allure()
-        .startStep('Contact-us')
-        .endStep(FormContactUsScreen.goToContactUs());
-
-        cy.allure()
-        .startStep('Complete Full Name')
-        .endStep(cy.get(FormContactUsScreen.inputFullName).type(this.dataForm.full_name_dev));
-
-        cy.allure()
-        .startStep('Complete Email Address')
-        .endStep(cy.get(FormContactUsScreen.inputEmail).type(this.dataForm.email_address));
-
-        cy.allure()
-        .startStep('Complete Company Name')
-        .endStep(cy.get(FormContactUsScreen.inputCompany).type(this.dataForm.company));
-
-        cy.allure()
-        .startStep('Complete Phone Number')
-        .endStep(cy.get(FormContactUsScreen.inputPhone).type(this.dataForm.phone));
-
-        cy.allure()
-        .startStep('Complete What service do you need?')
-        .endStep(cy.get(FormContactUsScreen.inputServicedev).select(this.dataForm.services.dev));
-       
-        if (sendMail === true) {
-            cy.allure()
-            .step('Validate Send')
-            .endStep(FormContactUsScreen.sendMail());
-            cy.wait(1000) //Ver como eliminar este paso
-            cy.allure()
-            .startStep('Validate Url Thanks')
-            .endStep(cy.url().should('be.equal', this.dataForm.urlthanks_dev));
-        }
-    })
-
 })
